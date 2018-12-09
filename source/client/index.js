0248a8a714bad8ecdab4c788f4833b4a6ea92a8e
Bill Burke <billstron@gmail.com>
2018-12-09 10:51:00 -0500

    adding heartbeats

--- source/client/index.js
+++ source/client/index.js
@@ -7,6 +7,8 @@ const server2 = { host: 'stun1.l.google.com', port: 19302 };
 
 module.exports = function Factory(uid, opts) {
   const DT_RESOLUTION = 10000;
+  const DT_KEEP_ALIVE = 60000;
+  const DT_OFFLINE_RETRY = 2000;
 
   const { stunServer, locationServer } = opts;
 
@@ -34,24 +36,43 @@ module.exports = function Factory(uid, opts) {
         });
     },
 
-    onMessage(msg, rinfo) {
+    onMessage(buffer, rinfo) {
       const { address, port } = rinfo;
       const friend = this.friends.find(({ address }) => {
         return address && address.public.host === rinfo.address && address.public.port === rinfo.port;
       });
       if (friend) {
-        const text = msg.toString('utf8');
-        console.log(`${friend.uid} says '${text}'`);
+        const msg = JSON.parse(buffer.toString('utf8'));
+        switch(msg.type) {
+          case 'text':
+            console.log(`${friend.uid} says '${msg.body}'`);
+            break;
+          case 'keep-alive':
+            console.log(`telling ${fridn.uid} that I'm alive`);
+            this.sendIsAlive(friend.uid);
+            break;
+          case 'is-alive':
+            console.log(`friend ${friend.uid} is alive`);
+            friend.online = true;
+            friend.lastTime = Date.now();
+            friend.tryCount = 0;
+            break;
+          default:
+            console.log(`${friend.uid}:`, msg);
+            break;
+        }
       }
     },
 
-    sendMessage(uid, message, callback) {
+    sendMessage(uid, data) {
       let friend;
+      const message = JSON.stringify(data);
       return Q.fcall(() => {
         friend = this.friends.find(friend => friend.uid === uid);
-        // if (friend.address) {
-        //   return friend.address;
-        // }
+        if (friend.online) {
+          return friend.address;
+        }
+        console.log('getting address from server', friend.uid);
         return request
           .get(`${locationServer}/${friend.uid}`)
           .then(reply => reply.data);
@@ -66,6 +87,30 @@ module.exports = function Factory(uid, opts) {
             console.log(`message sent to ${host}:${port}`);
           });
         })
+    },
+
+    sendIsAlive(uid) {
+      return this.sendMessage(uid, { type: 'is-alive' });
+    },
+
+    sendKeepAlive(uid) {
+      return this.sendMessage(uid, { type: 'keep-alive' });
+    },
+
+    processKeepAlives() {
+      const send = this.friends.filter(({ online = false, lastTime = 0, tryCount = 0 }) => {
+        if (online) {
+          return (lastTime + DT_KEEP_ALIVE) < Date.now();
+        }
+        return (lastTime + DT_OFFLINE_RETRY) < Date.now();
+      });
+
+      return Q.all(send.map((friend) => {
+        const { tryCount = 0 } = friend;
+        friend.tryCount = tryCount + 1;
+        friend.lastTime = Date.now();
+        return this.sendKeepAlive(friend.uid)
+      }));
     },
 
     addFriend(uid) {
@@ -88,8 +133,12 @@ module.exports = function Factory(uid, opts) {
       }, DT_RESOLUTION);
 
       setInterval(() => {
-        this.friends.forEach(({ uid }) => this.sendMessage(uid, 'ping'));
-      }, 2000);
+        this.processKeepAlives();
+      }, 100);
+
+      // setInterval(() => {
+      //   this.friends.forEach(({ uid }) => this.sendMessage(uid, 'ping'));
+      // }, 2000);
     },
 
     close() {
